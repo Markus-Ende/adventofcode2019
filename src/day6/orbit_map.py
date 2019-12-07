08e24079d40a9b43d103bc08b48c298078f2a1ab
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-07 17:05:16 +0100

    feat(day6): add solution for part 2

--- src/day6/orbit_map.py
+++ src/day6/orbit_map.py
@@ -1,21 +1,13 @@
-from anytree import Node, find, findall, RenderTree, AsciiStyle, PreOrderIter
+from anytree import Node, RenderTree, AsciiStyle, PreOrderIter, Walker
+from anytree.cachedsearch import find, findall
+
 
 def count_orbits(input):
-    root = Node("COM")
-    input_tokens = input.split()
-
-    while(len(input_tokens) > 0):
-        print(len(input_tokens), " to go")
-        token = input_tokens.pop(0);
-        parent = find(root, lambda node: node.name == token.split(")")[0])
-        if (parent != None):
-            Node(token.split(")")[1], parent=parent)
-        else:
-            input_tokens.append(token)
+    root = build_orbit_tree(input.split())
 
     # print(RenderTree(root, style=AsciiStyle()).by_attr())
 
-    count = 0;
+    count = 0
     for node in PreOrderIter(root):
         current_node = node
         while(current_node.parent != None):
@@ -23,3 +15,29 @@ def count_orbits(input):
             current_node = current_node.parent
 
     return count
+
+
+def build_orbit_tree(input_tokens):
+    root = Node("COM")
+    while(len(input_tokens) > 0):
+        # print(len(input_tokens), " to go")
+        token = input_tokens.pop(0)
+        parent = find(root, lambda node: node.name == token.split(")")[0])
+        if (parent != None):
+            Node(token.split(")")[1], parent=parent)
+        else:
+            input_tokens.append(token)
+    return root
+
+
+def count_transits(from_name, to_name, input):
+    root = build_orbit_tree(input.split())
+
+    # print(RenderTree(root, style=AsciiStyle()).by_attr())
+
+    from_node = find(root, lambda node: node.name == from_name).parent
+    to_node = find(root, lambda node: node.name == to_name).parent
+
+    walker = Walker()
+    (upwards, _, downwards) = walker.walk(from_node, to_node)
+    return (len(upwards) + len(downwards))
--- src/day6/test_orbit_map.py
+++ src/day6/test_orbit_map.py
@@ -1,11 +1,11 @@
 import pytest
-from day6.orbit_map import count_orbits
+from day6.orbit_map import count_orbits, count_transits
 from common.io import read
 
 
 @pytest.mark.parametrize("input,expected", [
     (
-    """
+        """
     COM)B
     B)C
     C)D
@@ -23,3 +23,27 @@ from common.io import read
 ])
 def test_count_orbits(input, expected):
     assert count_orbits(input) == expected
+
+
+@pytest.mark.parametrize("input,from_token,to_token,expected", [
+    (
+        """
+    COM)B
+    B)C
+    C)D
+    D)E
+    E)F
+    B)G
+    G)H
+    D)I
+    E)J
+    J)K
+    K)L
+    K)YOU
+    I)SAN
+    """, "YOU", "SAN", 4),
+    # uncomment on your own risk, it's very inefficient x-D
+    #(read("src/day6/input.txt"), "YOU", "SAN", 451)
+])
+def test_count_transits(input, from_token, to_token, expected):
+    assert count_transits(from_token, to_token, input) == expected
