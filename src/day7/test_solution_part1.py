cacdd9773fe5b5cd8ce095530d9291b414951e7d
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-08 13:57:28 +0100

    feat(day7): add solution for part 1

--- src/day7/test_solution_part1.py
+++ src/day7/test_solution_part1.py
@@ -1,9 +1,7 @@
-import pytest
-from day7.amplification_circuit import thruster_output
+from day7.amplification_circuit import thruster_output, max_thruster_output
+from common.io import read
 
 
-# @pytest.mark.parametrize("program,settings_sequence,expected", [
-#     ("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4, 3, 2, 1, 0], 43210)
-# ])
-# def test_thruster_output(program, settings_sequence, expected):
-#     assert thruster_output(program, settings_sequence) == expected
+def test_solution_part1():
+    intcode = read("src/day7/input.txt")
+    assert max_thruster_output(intcode) == 38500
