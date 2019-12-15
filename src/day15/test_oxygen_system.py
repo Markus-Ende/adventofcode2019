7a742279212a9538e7675816dc5da769a0a2eb26
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-15 18:46:03 +0100

    test(day15): add test for create_explorer

--- src/day15/test_oxygen_system.py
+++ src/day15/test_oxygen_system.py
@@ -25,8 +25,16 @@ def test_coords_to_map(input, expected):
 
 
 @pytest.mark.parametrize("input,expected", [
+    (intcode_input,
+     ([(0, -1, "#"), (0, 0, "s")], (0, 0)))
+])
+def test_create_explorer(input, expected):
+    assert create_explorer(input)(lambda: "1") == expected
+
+
+@pytest.mark.parametrize("input,expected", [
     (full_map,
      346)
 ])
 def test_oxygen_flooding(input, expected):
-    assert oxygen_flooding(full_map) == expected
+    assert oxygen_flooding(input) == expected
