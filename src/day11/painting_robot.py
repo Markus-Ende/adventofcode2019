5487b4694214dd5b1c882e190dddfcb2a50b12d4
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-15 20:40:46 +0100

    feat(day11): add solution for part 2

--- src/day11/painting_robot.py
+++ src/day11/painting_robot.py
@@ -1,14 +1,14 @@
 from day9.intcode_interpreter import run as run_intcode
+from common.map import coords_to_map
 
 
-def paint_hull(intcode):
+def paint_hull(intcode, starting_panel_color="0"):
     program_state = {"program": intcode}
     robot = (0, 0, "^")
     coords = {}
-    current_panel_color = "0"
+    current_panel_color = starting_panel_color
 
     while(program_state.get("ip", None) != -1):
-        current_panel_color = coords.get((robot[0], robot[1]), "0")
         output = []
         while(len(output) < 2 and program_state.get("ip", None) != -1):
             program_state = run_intcode(
@@ -26,4 +26,8 @@ def paint_hull(intcode):
             "v": lambda r: (r[0], r[1] + 1, "v"),
         }
         robot = move_forward[new_direction](robot)
-    return len(coords)
+        current_panel_color = coords.get((robot[0], robot[1]), "0")
+
+    return {
+        "amount_painted": len(coords),
+        "picture": coords_to_map([(c[0], c[1], coords[c]) for c in coords], "0")}
