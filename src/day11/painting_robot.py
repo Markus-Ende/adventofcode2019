63fba65166887471636450c9ddeb3c8417489372
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-15 20:03:22 +0100

    feat(day11): add solution for part 1

---
+++ src/day11/painting_robot.py
@@ -0,0 +1,29 @@
+from day9.intcode_interpreter import run as run_intcode
+
+
+def paint_hull(intcode):
+    program_state = {"program": intcode}
+    robot = (0, 0, "^")
+    coords = {}
+    current_panel_color = "0"
+
+    while(program_state.get("ip", None) != -1):
+        current_panel_color = coords.get((robot[0], robot[1]), "0")
+        output = []
+        while(len(output) < 2 and program_state.get("ip", None) != -1):
+            program_state = run_intcode(
+                program_state, True, [current_panel_color], lambda x: output.append(x))
+        if (program_state.get("ip", None) == -1):
+            break
+        coords[(robot[0], robot[1])] = str(output[0])
+        new_direction = "<^>v"[("<^>v".index(
+            robot[2]) - 1) % 4] if output[1] == 0 else "<^>v"[("<^>v".index(robot[2]) + 1) % 4]
+
+        move_forward = {
+            "<": lambda r: (r[0] - 1, r[1], "<"),
+            "^": lambda r: (r[0], r[1] - 1, "^"),
+            ">": lambda r: (r[0] + 1, r[1], ">"),
+            "v": lambda r: (r[0], r[1] + 1, "v"),
+        }
+        robot = move_forward[new_direction](robot)
+    return len(coords)
