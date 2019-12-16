fb28358c9019d0ae6616f7374a2ea91a8c1b0fcf
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-12-16 23:10:10 +0100

    feat(day13): add solution for part 2

--- src/day13/arcade_cabinet.py
+++ src/day13/arcade_cabinet.py
@@ -1,6 +1,7 @@
 from common.map import coords_to_map
 from os import system, name
 from day9.intcode_interpreter import run
+import math
 
 
 def render_screen_breakout(coords):
@@ -14,19 +15,27 @@ def render_screen_breakout(coords):
     return coords_to_map(coords, " ", mapping)
 
 
-def run_game(intcode, render, update_score=lambda x: None, interactive=False, input_func=input):
+def run_game(intcode, render, update_score=lambda x: None, insert_coin=False, interactive=False, input_func=input):
     tile_coords = {}
     current_partial_tile_coord = []
     score = 0
+    current_ball_x = -1
+    current_paddle_x = -1
 
     def render_buffered_output(output):
         nonlocal score
+        nonlocal current_ball_x
+        nonlocal current_paddle_x
         if (len(current_partial_tile_coord) == 2):
             if (current_partial_tile_coord[0] == -1):
                 score = output
                 update_score(score)
             else:
                 tile_coords[tuple(current_partial_tile_coord)] = output
+                if (output == 4):
+                    current_ball_x = current_partial_tile_coord[0]
+                if (output == 3):
+                    current_paddle_x = current_partial_tile_coord[0]
                 render([(tile[0], tile[1], tile_coords[tile])
                         for tile in tile_coords])
             current_partial_tile_coord.clear()
@@ -34,8 +43,17 @@ def run_game(intcode, render, update_score=lambda x: None, interactive=False, in
             current_partial_tile_coord.append(output)
         return
 
-    intcode = "2"+intcode[1:] if interactive else intcode
+    intcode = "2"+intcode[1:] if insert_coin else intcode
+
+    def paddle_input():
+        if (current_ball_x == current_paddle_x):
+            return 0
+        direction = math.trunc(math.copysign(
+            1, current_ball_x - current_paddle_x))
+        # print("current_ball_x", current_ball_x,
+        #       "current_paddle_x", current_paddle_x, "direction", direction)
+        return direction
 
     run({"program": intcode}, False, None, render_buffered_output,
-        input_func if interactive else None)
+        input_func if interactive else paddle_input)
     return score
